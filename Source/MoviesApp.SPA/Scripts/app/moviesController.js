6d5bba35fc4df13129d41456da1e7f040f2f25d9
Karan Sharma <cserkaran@gmail.com>
2017-02-02 22:01:06 +0530

    Movies poster and title getting loaded in  card tiles format.

--- Source/MoviesApp.SPA/Scripts/app/moviesController.js
+++ Source/MoviesApp.SPA/Scripts/app/moviesController.js
@@ -3,7 +3,7 @@ var app = angular.module('moviesApp',['ngMaterial','ngMdIcons']);
 app.controller('MoviesAppController', function ($scope, $http, MovieService, $timeout, $q) {
 
     $scope.movies = [];
-    MovieService.getAllMovies().then(function (movies)
+    MovieService.getCheapestPricedMovies().then(function (movies)
     {
         $scope.movies = movies;
         $scope.$apply()
